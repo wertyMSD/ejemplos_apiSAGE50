8e2a21fa6b71ac456ac4c6c602bbdc8912f2ac3f
infomsd <infomsd@gmail.com>
2023-04-26 11:42:39 +0200

    inicio

--- docs/obs.html/static/obsidian_core.js
+++ docs/obs.html/static/obsidian_core.js
@@ -10,7 +10,7 @@ var CONFIGURED_HTML_URL_PREFIX = "";
 var RELATIVE_PATHS = 1;
 var documentation_mode = 1;
 var tab_mode = !no_tab_mode;
-var gzip_hash = '262622833677457820219767458835723035080'                       // used to check whether the localStorage data is stale
+var gzip_hash = '141521073012985175393274514491566953024'                       // used to check whether the localStorage data is stale
 
 // global cache
 var fn_cache_ls_available = null;
